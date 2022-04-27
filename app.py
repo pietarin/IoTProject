ace6b1609f76048886eb42e770586534ff4b3407
Pietari pakarinen <Pietari.Pakarinen@tuni.fi>
2022-04-27 23:27:55 +0300

    second version of demo

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/data')
+def data():
+    dummydata = {
+        "Temperature": [25, 26, 27, 26, 28],
+        "Moisture": [75, 77, 76, 77, 76],
+        "Pump": 1,
+        "Water": 1
+    }
+    return dummydata
+
 @app.route('/')
 def home():
-    legend = 'Monthly Data'
-    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
-    values = [10, 9, 8, 7, 6, 4, 7, 8]
-    return render_template('index.html', values=values, labels=labels, legend=legend)
+    dummydata = data()
+    keys = list(dummydata.keys())
+    legend = ['Moisture', 'Temperature']
+    labels = [keys[0], keys[1]]
+    temperature = dummydata.get("Temperature")
+    moisture = dummydata.get("Moisture")
+    pump = dummydata.get("Pump")
+    water = dummydata.get("Water")
+    return render_template('index.html', temperature=temperature, moisture=moisture, pump=pump, water=water, labels=labels, legend=legend)
 
 if __name__ == '__main__':
     app.run(debug=True)
